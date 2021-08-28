11b1a4cc7c21763cce391cb99f7ef916cda945b2
Aman Kant Shakya <amankant19@gmail.com>
2021-08-29 02:54:13 +0530

    added alum page,conten can be elaborated WIP

---
+++ Alumni/1Script/Placement.php
@@ -0,0 +1,28 @@
+<h5><b>Placement Statistics</b></h5>
+<b>Batch of 2020- Tworcaz</b>
+<hr>
+
+<p>
+
+	<ul>
+		<br>
+		<li>Swiggy, Bangalore- 1</li>
+		<li>ADROSONIC Consultancy Services,Mumbai - 3 </li>
+		<li>Flipakart,Bangalore- 2</li>
+		<li>EXL Service, Guragon- 1</li>
+		<li>Mathworks,Hyderbad- 1</li>
+		<li>TCG Digital Solutions,Kolkata- 1</li>
+		<li>Global Analytics India,Chennai- 1</li>
+		<li>Arth Design Build India pvt ltd. ,Hyderbad- 5</li>
+		<li>Loadshare networks- 1</li>
+		<li>ANI Technologies, Bangalore- 1</li>
+		<li>BeeHyv Software solutions ,Hyderbad- 1</li>
+		<li>Auctus Advisors,New Delhi- 2</li>
+		<li>Uniworks Designs, Hyderbad- 2</li>
+		<li>GE Digital,B’lore- 1</li>
+		<li>Vconstruct ,Pune-1+2</li>
+		
+	</ul>
+</p>
+<br><br>
+<br><br><br>
